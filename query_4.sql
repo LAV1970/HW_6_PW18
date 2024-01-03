f0e9bb2fd7bd07813038aecfb47144bb6a75850e
lomakina0612 <lomakina0612@gmail.com>
2024-01-03 14:00:03 +0100

    request_4_1

--- query_4.sql
+++ query_4.sql
@@ -1,8 +1,2 @@
-SELECT AVG(CASE 
-            WHEN grade_name = 'неуд' THEN 2
-            WHEN grade_name = 'удовл' THEN 3
-            WHEN grade_name = 'хорошо' THEN 4
-            WHEN grade_name = 'отлично' THEN 5
-          END) AS average_grade
+SELECT AVG(fach) AS average_grade
 FROM grade;
-SELECT * FROM grade;
