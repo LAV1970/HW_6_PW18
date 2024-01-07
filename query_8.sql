d161075fc7c47a731ccb910639e2b8eb8ce1efdb
lomakina0612 <lomakina0612@gmail.com>
2024-01-07 12:41:59 +0100

    reguest_8_1

--- query_8.sql
+++ query_8.sql
@@ -1,4 +1,5 @@
 SELECT AVG(grade.grade_name) AS average_grade
 FROM grade
-JOIN student ON grade.student_id = student.student_id
-JOIN subject ON grade.subject_id = subject.subject_id;
+JOIN student ON grade.student = student.name
+JOIN professor_subject ON grade.subject_id = professor_subject.subject_id
+WHERE professor_subject.professor_id = 2;
